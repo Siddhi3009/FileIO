4cbaec18113203d4beafbc4d34451a0a36e7e800
Administrator <siddhiseth3009@gmail.com>
2020-10-28 16:08:07 +0530

    [Siddhi]Refactor FileIO Operation moved to FileIO class

---
+++ FileIODemo/FileIOOperations.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FileIODemo
+{
+    class FileIOOperations
+    {
+        /// <summary>
+        /// Checks if a file exists or not
+        /// </summary>
+        public void FileExists()
+        {
+            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
+            if (File.Exists(path))
+                Console.WriteLine("File exists");
+            else
+                Console.WriteLine(("File doesn't exists"));
+        }
+        /// <summary>
+        /// Reads all lines of a text file as array of strings and displays it on console
+        /// </summary>
+        public void ReadLines()
+        {
+            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
+            if (File.Exists(path))
+            {
+                string[] lines = File.ReadAllLines(path);
+                int lengthOfFile = lines.Length;
+                Console.WriteLine("Number of lines " + lengthOfFile);
+                for (int lineNo = 0; lineNo < lengthOfFile; lineNo++)
+                {
+                    Console.WriteLine(lines[lineNo]);
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No file");
+            }
+        }
+        /// <summary>
+        /// Reads all line of file as a string and displays on console.
+        /// </summary>
+        public void ReadText()
+        {
+            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
+            if (File.Exists(path))
+            {
+                string lines = File.ReadAllText(path);
+                Console.WriteLine(lines);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No file");
+            }
+        }
+        /// <summary>
+        /// Copies contents of one file to other.
+        /// </summary>
+        public void CopyFile()
+        {
+            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
+            string newPath = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\SampleCopy.txt";
+            if (File.Exists(path))
+            {
+                File.Copy(path, newPath);
+            }
+            else
+            {
+                Console.WriteLine("No file");
+            }
+        }
+        /// <summary>
+        /// Deletes file reffered by the path from the system.
+        /// </summary>
+        public void Delete()
+        {
+            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\SampleCopy.txt";
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+            }
+            else
+            {
+                Console.WriteLine("No file");
+            }
+        }
+        /// <summary>
+        /// Reads contents of a file using StreamReader.
+        /// </summary>
+        public void ReadFromStramReader()
+        {
+            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
+            if (File.Exists(path))
+            {
+                using (StreamReader sr = File.OpenText(path))
+                {
+                    string fileData = "";
+                    while ((fileData = sr.ReadLine()) != null)
+                        Console.WriteLine((fileData));
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No file");
+            }
+        }
+        /// <summary>
+        /// Appends Contents on a file using a StreamWriter.
+        /// </summary>
+        public void WriteUsingStreamWriter()
+        {
+            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
+            if (File.Exists(path))
+            {
+                using (StreamWriter streamWriter = File.AppendText(path))
+                {
+                    streamWriter.WriteLine("Hello World");
+                    streamWriter.Close();
+                    Console.WriteLine((File.ReadAllText(path)));
+                }
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("No file");
+            }
+        }
+    }
+}
--- FileIODemo/Program.cs
+++ FileIODemo/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.VisualBasic.FileIO;
 using System;
 using System.IO;
-
 namespace FileIODemo
 {
     class Program
@@ -10,14 +9,14 @@ namespace FileIODemo
         {
             Console.WriteLine("Hello World!");
             //File IO Operations
-            FileExists();
-            ReadLines();
-            ReadText();
-            CopyFile();
-            Delete();
-            Program p = new Program();
-            p.ReadFromStramReader();
-            p.WriteUsingStreamWriter();
+            FileIOOperations fileIOOperations = new FileIOOperations();
+            fileIOOperations.FileExists();
+            fileIOOperations.ReadLines();
+            fileIOOperations.ReadText();
+            fileIOOperations.CopyFile();
+            fileIOOperations.Delete();
+            fileIOOperations.ReadFromStramReader();
+            fileIOOperations.WriteUsingStreamWriter();
             //Serialization and Deserialization operations
             SerializationDeserialization serializationAndDeserialization = new SerializationDeserialization();
             serializationAndDeserialization.BinarySerialization();
@@ -27,108 +26,5 @@ namespace FileIODemo
             serializationAndDeserialization.XMLSerialize();
             serializationAndDeserialization.XmlDeSerialization();
         }
-
-        public static void FileExists()
-        {
-            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
-            if (File.Exists(path))
-                Console.WriteLine("File exists");
-            else
-                Console.WriteLine(("File doesn't exists"));
-        }
-        public static void ReadLines()
-        {
-            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
-            if (File.Exists(path))
-            {
-                string[] lines = File.ReadAllLines(path);
-                Console.WriteLine(lines[0]);
-                Console.WriteLine(lines[1]);
-                Console.WriteLine("Number of lines " + lines.Length);
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("No file");
-            }
-        }
-        public static void ReadText()
-        {
-            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
-            if (File.Exists(path))
-            {
-                string lines = File.ReadAllText(path);
-                Console.WriteLine(lines);
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("No file");
-            }
-        }
-        public static void CopyFile()
-        {
-            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
-            string newPath = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\SampleCopy.txt";
-            if (File.Exists(path))
-            {
-                File.Copy(path, newPath);
-            }
-            else
-            {
-                Console.WriteLine("No file");
-            }
-
-        }
-        public static void Delete()
-        {
-            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\SampleCopy.txt";
-            if (File.Exists(path))
-            {
-                File.Delete(path);
-            }
-            else
-            {
-                Console.WriteLine("No file");
-            }
-        }
-        public void ReadFromStramReader()
-        {
-            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
-            if (File.Exists(path))
-            {
-                using (StreamReader sr = File.OpenText(path))
-                {
-                    string fileData = "";
-                    while ((fileData = sr.ReadLine()) != null)
-                        Console.WriteLine((fileData));
-                }
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("No file");
-            }
-        }
-        public void WriteUsingStreamWriter()
-        {
-            string path = @"C:\Users\Administrator\Desktop\BridgeLabz Practice\33. File IO Operation\FileIO\FileIODemo\Sample.txt";
-            if (File.Exists(path))
-            {
-                using (StreamWriter streamWriter = File.AppendText(path))
-                {
-                    streamWriter.WriteLine("Hello World");
-                    streamWriter.Close();
-                    Console.WriteLine((File.ReadAllText(path)));
-                }
-                Console.ReadKey();
-            }
-            else
-            {
-                Console.WriteLine("No file");
-            }
-
-        }
-
     }
 }
